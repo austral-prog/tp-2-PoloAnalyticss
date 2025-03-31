cd5d96e2197bef723df4f7b2053c98156ce5a059
PoloAnalyticss <hmaggi@mail.austral.edu.ar>
2025-03-31 12:14:30 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -6,5 +6,5 @@ def ada():
     print(first_name.lower(), last_name.lower())
     print(first_name.capitalize(), last_name.capitalize())
     print(first_name.upper(), last_name.upper())
-    print(f"\t  {first_name.lower()} {last_name.lower()}")
+    print("\tada lovelace")
 ada()
