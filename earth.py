0242b131b79599e54290054ad2a4662e212236f6
PoloAnalyticss <hmaggi@mail.austral.edu.ar>
2025-03-31 12:12:12 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,11 @@
 def earth():
+
     x = "Bangladesh"
     y = "Barbados"
+
+    resultado1 = x < y  # True si Bangladesh va primero
+    resultado2 = y < x  # True si Barbados va primero
+
+    print(f"The result of {x} comes first in the dictionary than {y} is {resultado1}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {resultado2}.")
+earth()
